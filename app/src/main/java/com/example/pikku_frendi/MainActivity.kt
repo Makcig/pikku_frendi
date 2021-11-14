4500b91f0281c687bfc9714d2cc7f094eafb8e00
Maxim Igolkin <niidel@gmail.com>
2021-11-15 00:56:17 +0200

    First commit. Prject creating

---
+++ app/src/main/java/com/example/pikku_frendi/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.pikku_frendi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        // first comment
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
