23a72224e103f486c0dbdcf875b8c21c605fb06d
Yuliya Barai <Yuliya.Barai@edu.savonia.fi>
2021-12-07 14:53:24 +0200

    bottom_toolbar

--- app/src/main/java/com/example/pikku_frendi/MainActivity.kt
+++ app/src/main/java/com/example/pikku_frendi/MainActivity.kt
@@ -2,11 +2,13 @@ package com.example.pikku_frendi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        // first comment
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
