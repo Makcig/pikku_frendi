1f2a7ea0469735fd2628b66188a4320780bd96a0
Yuliya Barai <Yuliya.Barai@edu.savonia.fi>
2021-12-15 16:21:17 +0200

    buttons

--- app/src/main/java/com/example/pikku_frendi/CameraActivity.kt
+++ app/src/main/java/com/example/pikku_frendi/CameraActivity.kt
@@ -1,16 +1,13 @@
 package com.example.pikku_frendi
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import kotlinx.android.synthetic.main.activity_profile.*
 
 
 
 class CameraActivity : BaseActivity(1) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.login_page)
+        setContentView(R.layout.fragment_home)
         setupBottomNavigation()
     }
 }
