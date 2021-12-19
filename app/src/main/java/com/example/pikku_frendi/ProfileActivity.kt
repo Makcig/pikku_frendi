9eaf5f27406ef33a3b8e69db7657a8289c7d0125
Yuliya Barai <Yuliya.Barai@edu.savonia.fi>
2021-12-19 17:25:34 +0200

    sign out

--- app/src/main/java/com/example/pikku_frendi/ProfileActivity.kt
+++ app/src/main/java/com/example/pikku_frendi/ProfileActivity.kt
@@ -3,7 +3,11 @@ package com.example.pikku_frendi
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.TextView
+import androidx.appcompat.widget.PopupMenu
 import kotlinx.android.synthetic.main.activity_profile.*
+import kotlinx.android.synthetic.main.bottom_view.*
 
 
 class ProfileActivity : BaseActivity(2) {
@@ -12,9 +16,18 @@ class ProfileActivity : BaseActivity(2) {
         setContentView(R.layout.activity_profile)
         setupBottomNavigation()
 
-        edit_profile_btn.setOnClickListener{
+        edit_profile_btn.setOnClickListener {
             val intent = Intent(this, EditProfileActivity::class.java)
             startActivity(intent)
         }
+
+        val nick: TextView = findViewById<View>(R.id.nickname) as TextView
+        nick.setOnClickListener {
+            val popup = PopupMenu(this@ProfileActivity, nick)
+            popup.menuInflater.inflate(R.menu.popup_menu, popup.menu)
+            popup.show()
+
+        }
     }
 }
+
